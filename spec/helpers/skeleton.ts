fd300d355543958293172b4f2ebb706075ba58e9
dera- <alchemy.and.fairy@gmail.com>
2020-07-08 14:05:38 +0000

    follow akashic-3.0.0-beta.28

--- spec/helpers/skeleton.ts
+++ spec/helpers/skeleton.ts
@@ -2,11 +2,11 @@ import mock = require("./mock");
 
 function skeletonRuntime(gameConfiguration?: g.GameConfiguration) {
 	if (!gameConfiguration)
-		gameConfiguration = { width: 320, height: 320, main: "" };
+		gameConfiguration = { width: 320, height: 320, main: "", assets: {} };
 	var game = new mock.Game(gameConfiguration);
 	var scene = new g.Scene({ game });
 	game.pushScene(scene);
-	game._flushSceneChangeRequests();
+	game._handleSceneChanged();
 	return {
 		game: game,
 		scene: scene
